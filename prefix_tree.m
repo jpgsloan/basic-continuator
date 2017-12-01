f2dc69e2da6506fced37cbb8df8d6b1b523f8043
jsmattingly <jmatting@gmail.com>
2017-12-01 17:09:57 -0500

    Implemented prefix_tree class

---
+++ prefix_tree.m
@@ -0,0 +1,34 @@
+classdef prefix_tree
+    
+    properties
+        
+        note_value;
+        continuation_list;
+        children;
+        
+    end
+    
+    methods
+    
+        function [p] = parse(m)
+
+            % error checking stage wants only integer values between 0 and 127,
+                % with at least two values
+
+            % input 'm' for MIDI is MIDI pitch values already parsed from MIDI data
+
+            % output 'p' for prefixes is a multi-dimensional cell array
+
+            p = cell(length(m)-1,1);
+
+            for i = length(m)-1:-1:1
+    
+                p{i} = m(i:-1:1);
+                
+            end
+
+        end
+        
+    end
+    
+end
