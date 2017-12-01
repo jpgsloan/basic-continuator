81d43c7bab907f24791c2ce23947c3cac14d29fc
jsmattingly <jmatting@gmail.com>
2017-12-01 16:51:11 -0500

    Implement basic parse function

---
+++ parse.m
@@ -0,0 +1,16 @@
+function [p] = c_parse(m)
+
+% error checking stage wants only integer values between 0 and 127,
+% with at least two values
+
+% input 'm' for MIDI is MIDI pitch values already parsed from MIDI data
+
+% output 'p' for prefixes is a multi-dimensional cell array
+
+p = cell(length(m)-1,1);
+
+for i = length(m)-1:-1:1
+    
+    p{i} = m(i:-1:1);
+
+end
