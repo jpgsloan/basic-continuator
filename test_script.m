859be469618b58e0348e67c23a4e7f613e12ac03
jsmattingly <jmatting@gmail.com>
2017-12-11 17:59:35 -0500

    Fix error in contin-lists

--- test_script.m
+++ test_script.m
@@ -1,3 +1,4 @@
+clear;
 dummy_input_1 = [57 59 60 62];
 dummy_input_2 = [57 59 59 60];
 
@@ -6,3 +7,7 @@ test_tree = Prefix_tree;
 test_tree.parse(dummy_input_1)
 %%
 test_tree.parse(dummy_input_2)
+%%
+dummy_input_3 = [57 59 60 57];
+
+test_tree.parse(dummy_input_3);
