b5df7522b5daead6a124b144f844f5b989d693a5
jsmattingly <jmatting@gmail.com>
2017-12-16 23:52:50 -0500

    Implemented bucket sorting
    Imports Ken Schutte MIDI library; has functions for getting tempo and time signature from MIDI file, for calculating bucket size and boundaries, and one for sorting midi files into buckets by track and time

---
+++ buck_sort.m
@@ -0,0 +1,26 @@
+function [buck_notes] = buck_sort(midi, bars)
+
+[bpm, ts] = time_calc(midi);
+
+[notes, end_time] = midiInfo(midi, 0, []);
+end_time = max(end_time);
+
+[buck_lin] = buck_calc(bpm, ts, bars, end_time);
+
+buck_notes = cell(length(buck_lin)-1,length(midi.track));
+
+for i=1:length(midi.track)
+   
+    track_notes = notes(notes(:,1)==i,:);
+    if isempty(track_notes)
+        continue
+    end
+    
+    for j=1:length(buck_lin)-1
+       
+        buck_notes{j,i} = track_notes((track_notes(:,5)>=buck_lin(j)) & (track_notes(:,5)<=buck_lin(j+1)),:);
+        
+    end
+end
+
+end
---
+++ time_calc.m
@@ -0,0 +1,24 @@
+function [bpm, time_sig] = time_calc(midi)
+
+    % Function that returns the tempo (BPM) and time signature from a MIDI
+    % file
+
+midi_tempo = [];
+
+for i = 1:size(midi.track,2)
+
+    midi_tempo = midi.track(i).messages(find([midi.track(i).messages(:).type]==81)).data;
+    time_sig = midi.track(i).messages(find([midi.track(i).messages(:).type]==88)).data;
+    
+    if ~isempty(midi_tempo)
+        break
+    end
+end
+
+hex_base = [16^4;16^2;16^0];
+
+bpm = (60 * 10^6) / sum(midi_tempo .* hex_base);
+
+time_sig = time_sig(1);
+
+end
