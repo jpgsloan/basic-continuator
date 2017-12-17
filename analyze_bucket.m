623ca67ade18bb644d0766e2241d41b0d8d89be1
John Sloan <johnsloan@clarifai.com>
2017-12-17 00:18:17 -0500

    implement analyze_bucket

---
+++ analyze_bucket.m
@@ -0,0 +1,16 @@
+function [is_dense] = analyze_bucket(bucket, buck_dur, threshold) 
+% Calculate the note density within the bucket 
+
+is_dense = false;
+
+start_times = bucket(:,5);
+end_times = bucket(:,6);
+
+total_duration = sum(end_times - start_times);
+percent_dur = total_duration / buck_dur;
+
+if percent_dur > threshold 
+   is_dense = true; 
+end
+
+end
--- test_script.m
+++ test_script.m
@@ -18,3 +18,23 @@ b = Node(59, [], []);
 c = Node(60, [], []);
 
 out_seq = test_tree.generate([a b], 4);
+
+%% midi
+
+midi = readmidi('3a0e08597088225b13edaab26ce1e7d2.mid');
+notes = midiInfo(midi, 0, [4]);
+
+bool = analyze_bucket(notes(1:2,:), 1.5, 0.5)
+
+%track = midi.track(1);
+%notes(1:5,1:5)
+
+
+%% also, can do piano-roll showing velocity:
+[PR,t,nn] = piano_roll(notes,1);
+
+figure;
+imagesc(t,nn,PR);
+axis xy;
+xlabel('time (sec)');
+ylabel('note number');
