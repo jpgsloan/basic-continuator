97c7a418644ce5f4498a5a09bdc7591fd371f635
John Sloan <johnsloan@clarifai.com>
2017-12-18 14:24:37 -0500

    process in bulk

--- generate_dataset.m
+++ generate_dataset.m
@@ -1,6 +1,20 @@
-filename = 'test-midi/3a0e08597088225b13edaab26ce1e7d2.mid';
-midi = readmidi(filename);
-[bpm, ts] = time_calc(midi);
+addpath('lmd_full/5/');
+files = dir('lmd_full/5/*.mid');
+midi_cells = cell(size(files));
 
-output_data = contins_for_file(filename,1,4);
-output_data = analyze_contins(output_data,4,bpm,ts,true);
+dataset = [];
+
+for i = 1:size(files,1)    
+    filename = ['lmd_full/5/',files(i).name];
+    midi = readmidi(filename);
+    [bpm, ts] = time_calc(midi);
+
+    output_data = contins_for_file(filename,1,4);
+    output_data = analyze_contins(output_data,4,bpm,ts,false);
+    
+    dataset = [dataset; output_data];
+end
+
+
+
+
